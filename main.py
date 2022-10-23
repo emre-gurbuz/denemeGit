b1330a8b35ebc7f1e02087f1b956420526c0f341
emre gürbüz <01.hsyngrbz@gmail.com>
2022-10-23 20:22:58 +0300

    yeni methot dosyasi olusturuldu main duzenlendi

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import math
+import mettds
 
-sayi = int(input("Sayı gir :"))
+islem = input("işlem seç fakt(F) yada kare için(K) :")
 
-sonuc = math.factorial(sayi)
+sayı = int(input(f"{islem} sayı gir :"))
+
+if islem == "F":
+    sonuc = mettds.faktor(sayi)
+elif islem == "K":
+    sonuc = mettds.kare(sayi)
+else:
+    print("yanlis giris amk")
 
 print(sonuc)
---
+++ mettds.py
@@ -0,0 +1,8 @@
+from math import factorial, sqrt 
+def faktor(x):
+    ali = factorial(x)
+    return ali
+
+def kare(y):
+    veli = sqrt(y)
+    return veli
